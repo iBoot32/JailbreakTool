77cff2ce6f9978cf7a30ce9bab558ec74f69b163
iBoot32 <iboot32@gmail.com>
2019-01-02 17:50:21 -0800

    trying a test

---
+++ download.bat
@@ -0,0 +1,7 @@
+@echo off
+
+"C:/JBWork/partialzip.exe" "http://appldnld.apple.com/iOS5.1.1/041-4347.20120427.o2yov/iPhone2,1_5.1.1_9B206_Restore.ipsw" "038-4349-020.dmg" "C:/JBWork/ramdisk.dmg"
+"C:/JBWork/partialzip.exe" "http://appldnld.apple.com/iOS5.1.1/041-4347.20120427.o2yov/iPhone2,1_5.1.1_9B206_Restore.ipsw" "Firmware/dfu/iBSS.n88ap.RELEASE.dfu" "C:/JBWork/ibss.dfu"
+"C:/JBWork/partialzip.exe" "http://appldnld.apple.com/iOS5.1.1/041-4347.20120427.o2yov/iPhone2,1_5.1.1_9B206_Restore.ipsw" "Firmware/dfu/iBEC.n88ap.RELEASE.dfu" "C:/JBWork/ibec.dfu"
+"C:/JBWork/partialzip.exe" "http://appldnld.apple.com/iOS5.1.1/041-4347.20120427.o2yov/iPhone2,1_5.1.1_9B206_Restore.ipsw" "Firmware/all_flash/all_flash.n88ap.production/DeviceTree.n88ap.img3" "C:/JBWork/devicetree.img3"
+"C:/JBWork/partialzip.exe" "http://appldnld.apple.com/iOS5.1.1/041-4347.20120427.o2yov/iPhone2,1_5.1.1_9B206_Restore.ipsw" "kernelcache.release.n88" "C:/JBWork/kernelcache.release.n88"
